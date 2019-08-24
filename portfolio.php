23ae516c0023c756871651ada29e2596aed26df9
Shashank Shekhar <52148699+Shekhar-Shashank@users.noreply.github.com>
2019-08-24 17:33:35 +0530

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,218 @@
+<html>
+<head>
+<title> Portfolio </title>
+<link rel="stylesheet" href="Style.css">
+<link rel="stylesheet" 
+href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<link rel="style.sheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<!------------------Header Section----------------->
+<!-----<h1style="color:blue;">Shashank Shekhar</h1>---------->
+<section id="header">
+<div class="container text-center">
+<div class="user-box">
+<img src="img/7.jpg">
+<h1>  Shashank Shekhar</h1> 
+</div>
+</div>
+
+<div class="scroll-btn">
+<div class="scroll-bar"><a href="#about"><span> </span></a></div>
+</div>
+
+</section>
+<!--------------User-Info Section--------------------->
+<section id="user-info">
+
+<div class="nav-bar">
+<nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" right="50%" href="#"><img src="img/6.png" height="50" width="150" align="center"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	<i class="fa fa-bars"></i>
+    
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#top">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">ABOUT ME</a>
+      </li>
+      <!-----<li class="nav-item">
+        <a class="nav-link" href="#resume">PRICING</a>
+      </li>---->
+      <!---<li class="nav-item">
+        <a class="nav-link" href="#services">SERVICES</a>
+      </li>---->
+	  <li class="nav-item">
+        <a class="nav-link" href="#contact">CONTACT</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+
+
+ <!----------About------------>
+ 
+ <div class="about container" id="about">
+ <div class="row">
+   <div class="col-md-6 "> 
+      <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+     <div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="shashankshekhar-in"><a class="LI-simple-link" href='https://in.linkedin.com/in/shashankshekhar-in?trk=profile-badge'>Shashank Shekhar</a></div>
+   
+   </div> 
+ <div class="col-md-6 text-center">
+ <img src="img/7.jpg" width="300" height="300" class="profile-img">
+ </div>
+ <div class="col-md-10">
+ <h3> WHO AM I ?</h3>
+ <p>
+ Hardworking, reliable and self-motivated computer science and engineering student. I wish to use my technical and personal skills to contribute towards organizational goals and grow in the process.
+ </p>
+ <div class="skills-bar">
+ 
+ <p> Java</p>
+ <div class="progress">
+ <div class="progress-bar" style="width: 70%;"></div>
+ </div>
+ 
+ <p> Html</p>
+ <div class="progress">
+ <div class="progress-bar" style="width: 70%;"></div>
+ </div>
+ 
+ <p> Python</p>
+ <div class="progress">
+ <div class="progress-bar" style="width: 50%;"></div>
+ </div>
+ 
+ <p>Css </p>
+ <div class="progress">
+ <div class="progress-bar" style="width: 60%;"></div>
+ </div>
+ 
+<!--------- <p> Css</p>
+ <div class="progress">
+ <div class="progress-bar" style="width: 55%;"></div>
+ </div>-------->
+ </div>
+ </div>
+ </div>
+ </div>
+  
+    <!----------------Social Icons-------------->
+  <div class="social-icons">
+    
+    <ul>
+      <a href="https://www.linkedin.com/in/shashankshekhar-in/"><li><p>in</p></li></a>
+      <a href="https://github.com/Shekhar-Shashank"><li><p>Git</p></li></a>
+      <a href="https://www.facebook.com/in.shashank"><li><p>fb</p></li></a>
+    <!-------  <i class="fa fa-linkedin"></i>  <i class="fa fa-github"></i>   <i class="fa fa-facebook"></i>------->
+    </ul>
+  </div>
+ 
+    <!----------------Resume--------------->
+<div class="resume" id="resume">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<h3 class="text-center">Intership and Projects</h3>
+<ul class="timeline">
+<li>
+<h4><span>2019 - </span>Android App Developer and Framework Designer</h4>
+<p> Built an app from scratch in android studio using java and created a microservice/REST APIs using Python Flask. Implemented GET POST method on REST APIs. Parsed data using JSON and built database using SQLite.<br>
+<b>Company</b> - Indian Oil Private Ltd.<br>
+<b>Duration</b> - 6 Weeks [june 2019 to july 2019]<br>
+<b>Location</b> - Noida<br>
+ </p><li>
+ <h4><span> </span>Filter Android App</h4></li>
+ <li>
+<h4><span> </span>Tic Tac Toe Game</h4></li>
+<li>
+<h4><span> </span>Portfolio Website</h4>
+</li>
+<li>
+<h4><span> </span>Complaint Lodging app</h4>
+</li>
+</ul>
+</div>
+
+<div class="col-md-6">
+<h3 class="text-center">Education</h3>
+<ul class="timeline">
+<li>
+<h4><span>2020- </span>Graduation</h4>
+<p> <b> University </b>Galgotias University<br>
+<b>Duration</b> - 2016 - 2020<br>
+<b>CGPA</b> - 7.64<br>
+ </p>
+ </li>
+ <li>
+<h4><span>2015- </span>Intermediate</h4>
+<p>
+  <b> Institution </b> College of Commerce, Patna<br>
+    <b>Duration</b> - 2013 - 2015<br>
+<b>Percentage</b> - 71.8%<br>
+<b>Board</b> - BSEB <br>
+ </p>
+</li>
+<li>
+<h4><span>2013- </span>High School</h4>
+  <p><b>School </b> Satyam International, Patna<br>
+  <b>Duration</b> - 2011 - 2013<br>
+<b>CGPA</b> - 10<br>
+<b>Board</b> - CBSE <br>
+ </p>
+</li>
+
+
+</li>
+</ul>
+</div>
+
+</div>
+</div>
+</div>	
+
+<!-----------------Contact--------------->
+
+<div class="contact" id="contact">
+<div class="container text-center">
+<h1 style="color:white;"> Contact Me</h1>
+<div class="row" align="center">
+<div class="col-md-2">
+  </div>  
+<div class="col-md-3">
+<i class="fa fa-phone"></i>
+<p style="color:white;">9599176871</p>
+</div>
+<div class="col-md-3">
+<i class="fa fa-envelope"></i>
+<p style="color:white;">shekhars027@gmail.com</p>
+</div>
+  
+
+</div>
+<button type="button" class="btn btn-primary"><i class="fa fa-download"></i><a href="https://www.dropbox.com/s/tc5iyzejvn0x1tp/resume%20simple.pdf?dl=0">Resume</a></button>
+<button type="button" class="btn btn-primary"><i class="fa fa-rocket"></i><a href="https://mail.google.com/mail/u/0/#inbox?compose=new">Hire Me</a></button>
+</div>
+</div>
+</section>
+
+  <!----------------Smooth Scroll-------------------->
+  <script src="smooth-scroll.js"></script>
+  <script>
+	var scroll = new SmoothScroll('a[href*="#"]');
+</script>
+
+</body>
+</html>
